97d170feacc6d6992f2b8ccae1bee2718c4efece
Khang <54690164+aktran321@users.noreply.github.com>
2026-02-21 03:45:00 -0500

    Create WN11-CC-000315.ps1

---
+++ remediations/WN11-CC-000315.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures STIG WN11-CC-000315 is satisfied. “Always install with elevated privileges” must be Disabled
+
+.NOTES
+    Author          : Khang Tran
+    LinkedIn        : https://www.linkedin.com/in/khang-tran-622a44163/
+    GitHub          : https://github.com/aktran321
+    Date Created    : 2/21/2026
+    Last Modified   : 2/21/2026
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000315
+
+.TESTED ON
+    Date(s) Tested  : 2/21/2026
+    Tested By       : Khang Tran
+    Systems Tested  : Windows 11 Pro
+    PowerShell Ver. : 5.1.26100.7705
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\WN11-CC-000315.ps1
+#>
+# WN11-CC-000315
+$uPath = "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Installer"
+if (-not (Test-Path $uPath)) { New-Item -Path $uPath -Force | Out-Null }
+
+New-ItemProperty -Path $uPath -Name $name -PropertyType DWord -Value 0 -Force | Out-Null
+$path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Installer"
+$name = "AlwaysInstallElevated"
+$desired = 0  # Disabled
+
+if (-not (Test-Path $path)) { New-Item -Path $path -Force | Out-Null }
+New-ItemProperty -Path $path -Name $name -PropertyType DWord -Value $desired -Force | Out-Null
+
+# Verify
+(Get-ItemProperty -Path $path -Name $name).$name
